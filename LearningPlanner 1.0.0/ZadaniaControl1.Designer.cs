4c77a4c6e351cd6a272023f2d29d7af02b2e8eb9
Jakub Benzef <destroyeddestroy@gmail.com>
2019-04-22 20:38:18 +0200

    Dodano lupke

--- LearningPlanner 1.0.0/ZadaniaControl1.Designer.cs
+++ LearningPlanner 1.0.0/ZadaniaControl1.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -39,9 +41,9 @@
             this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(200, 113);
+            this.label1.Size = new System.Drawing.Size(150, 92);
             this.label1.TabIndex = 0;
             this.label1.Text = "Zadania";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -52,24 +54,34 @@
             // 
             this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.pictureBox1.Image = global::LearningPlanner_1._0._0.Properties.Resources.Zadanie_icon;
-            this.pictureBox1.Location = new System.Drawing.Point(5, 34);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0, 12, 0, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 28);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0, 10, 0, 10);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 47);
+            this.pictureBox1.Size = new System.Drawing.Size(45, 38);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LearningPlanner_1._0._0.Properties.Resources.Znajdz_zadanie_icon;
+            this.pictureBox2.Location = new System.Drawing.Point(47, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
             // ZadaniaControl1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ZadaniaControl1";
-            this.Size = new System.Drawing.Size(200, 113);
+            this.Size = new System.Drawing.Size(150, 92);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -78,5 +90,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
